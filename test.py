0431fbbdcf347da53b938f137c9d406b29f4ea00
= <=>
2022-02-20 21:28:49 +0500

    sfsdf

--- test.py
+++ test.py
@@ -37,28 +37,22 @@ def sendDocument(filename):
     files = {'document': open(f"./{filename}", 'rb')}
     url = "https://api.telegram.org/bot5243536300:AAFQrJVeFQKChsh8QEbwA-pZ4k2I2gOkLAU/sendDocument?chat_id=2083029174"
     res = requests.post(url, files=files)
-
-
-def check_by_partial_link_text(path):
-    try:
-        driver.find_element(By.PARTIAL_LINK_TEXT, path)
-    except e.NoSuchElementException:
-        return False
-    return True
-
-
 # print("Deleting extra files")
 # if platform == 'linux' or platform == 'linux2':
 #     os.system('ls')
 
 
 driver.get("https://www.whatsapp.com/download/")
-time.sleep(3)
 sendDocument("test.png")
-if check_by_partial_link_text("ACCEPT"):
+try:
     driver.find_element(By.PARTIAL_LINK_TEXT, "ACCEPT").click()
-if check_by_partial_link_text("64-bit"):
+except e.NoSuchElementException:
+    print("Accept Button Not Found")
+sendDocument("test.png")
+try:
     driver.find_element(By.PARTIAL_LINK_TEXT, "64-bit").click()
+except e.ElementClickInterceptedException:
+    driver.find_element(By.PARTIAL_LINK_TEXT, "ACCEPT").click()
 print("Download Button Clicked")
 sendDocument("test.png")
 dlwait = False
@@ -78,6 +72,6 @@ while True:
         else:
             dlwait = False
     if not dlwait:
-        print(f"File Downloaded: {downloadingFile}")
         sendDocument(downloadingFile)
+        print(f"File Downloaded: {downloadingFile}")
         break
