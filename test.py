e487d909022f37a82131e2906fce2ee50566dc9a
= <=>
2022-02-20 12:36:14 +0500

    dsff

--- test.py
+++ test.py
@@ -3,9 +3,11 @@ import os
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-
+from sys import platform
 
 options = webdriver.ChromeOptions()
+options.add_argument("--headless --sandbox --disable-gpu")
+
 
 preferences = {"download.default_directory": os.getcwd(),
                "safebrowsing.enabled": "false"}
@@ -14,30 +16,32 @@ options.add_experimental_option("prefs", preferences)
 
 driver = webdriver.Chrome(service=Service(
     ChromeDriverManager().install()), options=options)
+print("Headless Chrome Driver Initiated!")
+
+print("Deleting extra files")
+if platform == 'linux' or platform == 'linux2':
+    os.system(
+        'rm -rf -v !("bot.py"|"Procfile"|"requirements.txt"|"runtime.txt"|"test.py")')
 
 driver.get("https://www.whatsapp.com/download/")
 driver.find_element_by_xpath(
     '//*[@id="content-wrapper"]/div/div/div/div/div/div/div[2]/div/div/div/div/div/a').click()
+print("Download Button Clicked")
 
-
-def every_downloads_chrome(driver):
-    if not driver.current_url.startswith("chrome://downloads"):
-        driver.get("chrome://downloads/")
-    time.sleep(3)
-    return driver.execute_script("""
-        var items = document.querySelector('downloads-manager')
-            .shadowRoot.getElementById('downloadsList').items;
-        if (items[0].state === 'COMPLETE') return "Completed";
-        else if(items[0].state === 'IN_PROGRESS') return items[0].progressStatusText;
-        else return items[0].state;
-        """)
-
-
-print("Starting loop...")
+dlwait = False
 
 while True:
-    if every_downloads_chrome(driver) == 'CANCELLED':
-        print("Cancelled")
-    else:
-        print(every_downloads_chrome(driver))
     time.sleep(5)
+    files = os.listdir(os.getcwd())
+    for fname in files:
+        if fname.endswith('.crdownload'):
+            downloadingFile = fname[:-11]
+            print(
+                f"{downloadingFile}: {int(os.path.getsize(fname) / (1024 * 1024))}MB Downloaded")
+            dlwait = True
+            break
+        else:
+            dlwait = False
+    if not dlwait:
+        print(f"File Downloaded: {downloadingFile}")
+        break
