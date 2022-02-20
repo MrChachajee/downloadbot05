bccfcbd2efb200fa39efacb5527ed8debf29f332
= <=>
2022-02-20 22:06:18 +0500

    ddfsd

--- test.py
+++ test.py
@@ -31,24 +31,21 @@ driver = webdriver.Chrome(service=Service(
     ChromeDriverManager().install()), options=options)
 print("Headless Chrome Driver Initiated!")
 
+x = 0
+
 
 def sendDocument(filename):
+    x += 1
     driver.save_screenshot("test.png")
     files = {'document': open(f"./{filename}", 'rb')}
-    url = "https://api.telegram.org/bot5243536300:AAFQrJVeFQKChsh8QEbwA-pZ4k2I2gOkLAU/sendDocument?chat_id=2083029174"
+    url = "https://api.telegram.org/bot5243536300:AAFQrJVeFQKChsh8QEbwA-pZ4k2I2gOkLAU/sendDocument?chat_id=2083029174&caption=Number: " + x
     res = requests.post(url, files=files)
-# print("Deleting extra files")
-# if platform == 'linux' or platform == 'linux2':
-#     os.system('ls')
+    print(f"File Sended: {filename}")
 
 
 driver.get("https://www.whatsapp.com/download/")
 sendDocument("test.png")
-try:
-    driver.find_element(By.PARTIAL_LINK_TEXT, "ACCEPT").click()
-except e.NoSuchElementException:
-    print("Accept Button Not Found")
-sendDocument("test.png")
+
 try:
     element = driver.find_element(By.PARTIAL_LINK_TEXT, "64-bit")
     driver.execute_script("arguments[0].click();", element)
@@ -60,9 +57,6 @@ except e.ElementClickInterceptedException:
 print("Download Button Clicked")
 sendDocument("test.png")
 dlwait = False
-
-# downloadingFile = "runtime.txt"
-
 while True:
     time.sleep(5)
     files = os.listdir(os.getcwd())
